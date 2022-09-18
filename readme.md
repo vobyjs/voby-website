de97a5936ae040a06564b1266f63d24ff5304b71
LennyAnders <hey@lenny.fyi>
2022-09-18 15:34:12 +0200

    Revert "initial playground commit"
    This reverts commit 994471ad249aa3f910eb7dd99b8482d2ff3ff9ae.

--- readme.md
+++ readme.md
@@ -2,20 +2,6 @@
 
 The website served at [`voby.dev`](https://voby.dev).
 
-## Setup
+---
 
-```
-npm i
-```
-
-## Development
-
-```
-npm start
-```
-
-## Build
-
-```
-npm run build
-```
+//TODO
--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import './main.scss';
-
-import { render } from 'voby';
-
-import { Header } from './Header';
-import { Playground } from './Playground';
-
-render([Header, Playground], document.body);
